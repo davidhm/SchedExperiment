bb646ab16854b94b6222b8067150e0d5e73203f4
davidhm <david.hm.1994@gmail.com>
2017-08-15 20:41:39 +0200

    This is starting to take shape

--- Website/js/main.js
+++ Website/js/main.js
@@ -1,9 +1,24 @@
-var input = $("#subjects .typeahead");
-input.typeahead({
-  source: [
-    {id: "test1", name: "Test 1"},
-    {id: "test2", name: "Test 2"},
-  ],
-  autoSelect: true,
-  hint: true
+function addSubject(subject) {
+  var oldHtml = $("#selectedSubjects").html();
+  $("#selectedSubjects").html(
+    oldHtml + '<div class="chip">' + subject +
+    '<i class="close material-icons">close</i>'+
+    '</div>'
+  );
+}
+
+$(document).ready(function(){
+  $("input.autocomplete").autocomplete({
+    data: {
+      "Apple": null,
+      "Microsoft": null,
+      "Google": null
+    },
+    limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+    onAutocomplete: function(val) {
+      addSubject(val);
+    },
+    minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+  });
+  $("select").material_select();
 });
